7025cc44de5fdffc39a2102c67c87fa854181dfe
akapchuk <Kapchuk768@gmail.com>
2021-03-01 23:20:09 +0300

    create project

---
+++ TortyMozyrApp/TortyMozyrApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TortyMozyrApp
+//
+//  Created by Саша Капчук on 1.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
